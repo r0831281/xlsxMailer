aa83ee37742d300df1e2d536e76b519fd22e229e
Jonas <r0831281@student.thomasmore.be>
2023-11-17 21:07:12 +0100

    done-ish

--- csvtest.py
+++ csvtest.py
@@ -4,19 +4,18 @@
 
 
 import argparse
-import pandas as pd
+import time
+import pandas as pd 
 import smtplib
 import getpass
 from O365 import Message, MSGraphProtocol, Account, FileSystemTokenBackend
 
 
-scopes = ['https://graph.microsoft.com/.default/Mail.ReadWrite', 'https://graph.microsoft.com/Mail.Send', 'basic']
-
 
 html_template =     """ 
             <html>
             <head>
-                <title></title>
+                <title>Site data</title>
             </head>
             <body>
                     {}
@@ -26,7 +25,7 @@ html_template =     """
 
 parser = argparse.ArgumentParser(description='get site data.')
 parser.add_argument('site', metavar='-s', type=int, nargs='+',
-                    help='site name')
+                    help='site number')
 parser.add_argument('xlxs', metavar='-x', type=str, nargs='+', help='xlxs file location')
 parser.add_argument('--version', '-V', action='version', version='%(prog)s 1.0')
 parser.add_argument('--verbose', '-v', action='count', default=0)
@@ -34,17 +33,17 @@ parser.add_help = True
 
 
 protocol = MSGraphProtocol()
+scopes = protocol.get_scopes_for(['basic', 'message_all'])
 
-
-def authenticate_to_outlook(sendr):
+def authenticate_to_outlook():
     try:
            
-            client_secret = 'MC~8Q~82rBtOVn2SWA0bkJ8nPJZJUwdpCQWNoa_a'
+            client_secret = ' '
             client_id = 'ff91f935-c699-4f05-8844-0570bd88b673'
             credentials = (client_id, client_secret)
             token_backend = FileSystemTokenBackend(token_path='/tokenStore', token_filename='my_token.txt')
-            o365_auth = Account(credentials = credentials, auth_flow_type='credentials', protocol=protocol, tenant_id='f8cdef31-a31e-4b4a-93e4-5f571e91255a')
-            o365_auth.authenticate(scopes=scopes, token_backend=token_backend, account_email=sendr)
+            o365_auth = Account(credentials)
+            o365_auth.authenticate(scopes=scopes, token_backend=token_backend)
             return o365_auth
     except Exception as e:
         print("An error occurred: ", e)
@@ -61,29 +60,41 @@ def getConfirmation():
    
 
 def getsendrAndRecievr():
-    sendr = input("Enter sender e-mail address: ")
+    #sendr = input("Enter sender e-mail address: ")
     recievr = input("Enter reciever e-mail address: ")
-    return sendr, recievr
+    return recievr
 
 def getBySite(site, xlxs):
     df = pd.read_excel(xlxs, index_col=0, sheet_name=None)
     cols =  ['data1', 'data3', 'data4']
-    outData = df["Sheet1"].loc[site, cols]._append(df['Sheet2'].loc[site, cols])
+    data1 = df["Sheet1"].loc[site, cols]
+    data1.dropna(inplace=True)
+    try:
+        data1 = data1.to_frame()
+        data1 = data1.transpose()
+    except(Exception):
+        print("error")    
+    print(data1)
+    data2 = df['Sheet2'].loc[site, cols]
+    try:
+        data2 = data2.to_frame()
+        data2 = data2.transpose()
+    except(Exception):
+        print("error")
+    outData = pd.concat([data1, data2], ignore_index=True)
+    print(data2)
     return outData
 
 
-if __name__ == '__main__':
-    print(
-    """
+def main():
+    print("""
 __  __  __  ____  __  __ _ _            ___      _            ___      _ _           _             
 \ \/ / / / / _\ \/ / / _(_) |_ ___     /   \__ _| |_ __ _    / __\___ | | | ___  ___| |_ ___  _ __ 
  \  / / /  \ \ \  /  \ \| | __/ _ \   / /\ / _` | __/ _` |  / /  / _ \| | |/ _ \/ __| __/ _ \| '__|
  /  \/ /____\ \/  \  _\ \ | ||  __/  / /_// (_| | || (_| | / /__| (_) | | |  __/ (__| || (_) | |   
 /_/\_\____/\__/_/\_\ \__/_|\__\___| /___,' \__,_|\__\__,_| \____/\___/|_|_|\___|\___|\__\___/|_|   
                    By Jonas Quintiens                                                                                                                                                                         
-
-    """
-    )
+""")
     try:
         args = parser.parse_args()
         if not args.site or not args.xlxs:
@@ -91,31 +102,33 @@ __  __  __  ____  __  __ _ _            ___      _            ___      _ _      
         else:
             data = getBySite(args.site[0], args.xlxs[0])
             print(data)
-            sendr, recievr = getsendrAndRecievr()
+            recievr = getsendrAndRecievr()
             print( 
 """
-Mailer name is: %s
 reciever is %s
 """
-                  %(sendr,recievr))
+                  %(recievr))
             while not getConfirmation():
-                sendr, recievr = getsendrAndRecievr()
-
-            final_html_data = html_template.format(data.to_html(index=False))
-            m = authenticate_to_outlook(sendr)
+                recievr = getsendrAndRecievr()
+            final_html_data = html_template.format(data.to_html())
+            m = authenticate_to_outlook()
             if m.is_authenticated:
-                m.new_message()
+                a = m.new_message()
                 print("sending mail")
-                m.to.add(recievr)
-                m.subject = "test"
-                m.body = final_html_data
-                m.send()
+                a.to.add(recievr)
+                a.subject = "test"
+                a.body = final_html_data
+                a.send()
                 print("mail sent")
             else:
                 print("not authenticated")
             
     except Exception as e:
-        print(e)
-        parser.print_help()
+            print(e)
+            parser.print_help()
 
     
+
+
+if __name__ == '__main__':
+    main()
