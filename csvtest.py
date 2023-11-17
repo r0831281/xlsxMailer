376162e3140d8200ebaca9b701a8bca2dfb4c20c
Jonas <r0831281@student.thomasmore.be>
2023-11-17 11:40:50 +0100

    working loop

--- csvtest.py
+++ csvtest.py
@@ -13,16 +13,31 @@ parser.add_argument('--version', '-V', action='version', version='%(prog)s 1.0')
 parser.add_argument('--verbose', '-v', action='count', default=0)
 parser.add_help = True
 
+def getConfirmation():
+        a = input("do you want to continue? (y/n): ")
+        if a == 'y':
+            return True
+        elif a == 'n':
+            return False
+        else:
+            print("invalid input")
 
+    
+
+def getsendrAndRecievr():
+    sendr = input("Enter sender e-mail address: ")
+    recievr = input("Enter reciever e-mail address: ")
+    return sendr, recievr
 
 def getBySite(site, xlxs):
-    df = pd.read_excel(xlxs, index_col="site", sheet_name=None)
+    df = pd.read_excel(xlxs, index_col=0, sheet_name=None)
     cols =  ['data1', 'data3', 'data4']
     outData = df["Sheet1"].loc[site, cols]._append(df['Sheet2'].loc[site, cols])
     return outData
 
 def parseMail(data, sendr, recievr):
     return data
+
 
 if __name__ == '__main__':
     try:
@@ -32,20 +47,20 @@ if __name__ == '__main__':
         else:
             data = getBySite(args.site[0], args.xlxs[0])
             print(data)
-            sendr = input("Enter sender e-mail address: ")
-            recievr = input("Enter reciever e-mail address: ")
+            sendr, recievr = getsendrAndRecievr()
             print( 
 """
 Mailer name is: %s
 reciever is %s
 """
                   %(sendr,recievr))
-            input("do you want to continue? (y/n): ")
-            authenticate_to_gmail(sendr)
-            if input == 'y':
-                parseMail(data, sendr, recievr)
-            else:
-                exit(0)
+            while not getConfirmation():
+                sendr, recievr = getsendrAndRecievr()
+            print("sending mail")
+            data = parseMail(data, sendr, recievr)
+
+
+
     except Exception as e:
         print(e)
         parser.print_help()
